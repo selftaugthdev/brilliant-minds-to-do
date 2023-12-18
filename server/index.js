9046b1a9a52b429003e79bb338098347e5a64340
Thierry De Belder <thierrydebelder@Laptop-van-Thierry.local>
2023-12-18 15:36:42 +0100

    fix error codes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { config } from "dotenv";
-import mariadb from "mariadb"
+import mariadb from "mariadb";
 config();
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "localhost";
 const app = express();
 
 const pool = mariadb.createPool({
@@ -13,47 +12,43 @@ const pool = mariadb.createPool({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     connectionLimit: 5
-})
+});
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-        let connection;
-        try {
-            connection = await pool.getConnection();
-            const data = await connection.query(
-                "SELECT * FROM ideas"
-                )
-            res.send(data)
-        } catch(err) {
-            throw err;
-            //console.error("Error: ", err);
-        } finally {
-            if (connection) connection.end();
-        }
-})();
-
-app.get('/ideas/:id', async (req, res) => {
     let connection;
     try {
         connection = await pool.getConnection();
-        const prepare = await connection.prepare(
-            "SELECT * FROM ideas WHERE id = ?"
-        )
-        const data = await prepare.execute([req.params.id])
-        res.send(data)   
-    } catch (error) {
-        throw error;
+        const data = await connection.query("SELECT * FROM ideas");
+        res.send(data);
+    } catch(err) {
+        console.error("Error: ", err);
+        res.status(500).send('Server Error');
     } finally {
         if (connection) connection.end();
     }
-  })
+});
 
-app.post('/create', (req, res) => {
-    console.log(req.body);
-    res.send("Idea received: " + JSON.stringify(req.body));
-})
+app.get("/show-ideas/:id", async (req, res) => {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const prepare = await connection.prepare("SELECT * FROM ideas WHERE id = ?");
+        const data = await prepare.execute([req.params.id]);
+        res.send(data);
+    } catch (error) {
+        console.error("Error: ", error);
+        res.status(500).send('Server Error');
+    } finally {
+        if (connection) connection.end();
+    }
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+/* app.post('/create-ideas', async (req, res) => {
+    //code
+}) */
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});

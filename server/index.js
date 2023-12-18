df06bb51bba0ac62331b48c0bed639603348e346
Thierry De Belder <thierrydebelder@Laptop-van-Thierry.local>
2023-12-18 14:43:49 +0100

    Add correct code to index.js file

--- server/index.js
+++ server/index.js
@@ -1,39 +1,58 @@
-const express = require('express');
+import express from "express";
+import { config } from "dotenv";
+import mariadb from "mariadb"
+config();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
 const app = express();
-import * as dotenv from 'dotenv'
-import {config} from "dotenv";
-dotenv.config()
-const port = process.env.PORT || 8000;
-const path = require('path');
-import mariadb from 'mariadb'
-config()
 
-server.get("/show-all", async (req, res) => {
-    // database connection
-    // execute query
-        // send response with data
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
 })
 
-// Middleware to parse request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('This is the Index page')
-})
+app.get('/', async (req, res) => {
+        let connection;
+        try {
+            connection = await pool.getConnection();
+            const data = await connection.query(
+                "SELECT * FROM ideas"
+                )
+            res.send(data)
+        } catch(err) {
+            throw err;
+            //console.error("Error: ", err);
+        } finally {
+            if (connection) connection.end();
+        }
+})();
+
+app.get('/ideas/:id', async (req, res) => {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const prepare = await connection.prepare(
+            "SELECT * FROM ideas WHERE id = ?"
+        )
+        const data = await prepare.execute([req.params.id])
+        res.send(data)   
+    } catch (error) {
+        throw error;
+    } finally {
+        if (connection) connection.end();
+    }
+  })
 
 app.post('/create', (req, res) => {
     console.log(req.body);
     res.send("Idea received: " + JSON.stringify(req.body));
 })
-
-app.get('/ideas', (req, res) => {
-    let connection;
-    try {
-        
-    } catch (error) {
-        
-    }
-  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
